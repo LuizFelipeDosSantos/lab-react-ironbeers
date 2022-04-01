eb1548bd3a56d0b2b16f57f4dcdf228f11866be1
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-04-01 15:06:47 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Beers } from './components/Beers';
 import { Home } from './components/Home';
@@ -9,15 +9,29 @@ function ErrorHandle() {
   return <div>Sorry, we don't have this page!</div>
 }
 
+function LayoutComponent() {
+  return (
+    <div>
+      <Link to="/"> ⌂ HOME </Link>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <Routes >
         <Route path='*' element={<ErrorHandle />} />
         <Route path="/" element={<Home />} />
-        <Route path="/beers" element={<Beers />} />
-        <Route path="/random-beer" element={<RandomBeer />} />
-        <Route path="/new-beer" element={<NewBeer />} />
+        <Route element={ <LayoutComponent /> }>   {/* wrapper for all subsequent components */}
+          <Route path="/beers" element={<Beers />} />
+          <Route path="/random-beer" element={<RandomBeer />} />
+          <Route path="/new-beer" element={<NewBeer />} />
+        </Route>
       </Routes>
     </div>
   );
