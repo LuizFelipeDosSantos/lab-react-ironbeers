e2d07099970d2d5d5196683cad892b319d5ce383
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-04-01 16:38:18 +0200

    iteration 4 done

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,3 +1,39 @@
+import { useEffect, useState } from "react";
+
+
 export function RandomBeer() {
-    return <div>RandomBeer</div>
+    const [beer, setBeer] = useState({});
+
+    useEffect(() => {
+        async function fetchRandomBeer() {
+            const res = await fetch("https://ih-beers-api2.herokuapp.com/beers/random");
+            const data = await res.json();
+            setBeer(data);
+        }
+        fetchRandomBeer();
+    }, []);
+
+    return (
+        <div className="flex">
+        {/* Error: undefined beer -> therefore first check if it exists */}
+        {beer ? ( 
+            <>
+                <div>
+                    <img src={beer.image_url} alt={beer.name} height={300}/>
+                </div>
+                <div className="beer-info">
+                    <h2>{beer.name}</h2>
+                    <h3>{beer.tagline}</h3>
+                    <p><b>{beer.first_brewed}</b></p>
+                    <p>{beer.attenuation_level}</p>
+                    <p>{beer.description}</p>
+                    <p><b>Created by: </b>{beer.contributed_by}</p>
+                </div>
+            </>) :  (
+                "🍻Loading..."
+            )        
+        }
+        </div>
+        
+    )
 }
